0d209ba5112c6f6a6a798eafff2eccf85b7a29e0
Romain Bossart <romain.bossart@ge.com>
2017-09-22 09:49:19 +0200

    3. Publish and Receive messages

--- main.go
+++ main.go
@@ -8,15 +8,72 @@ import (
 )
 
 func main() {
+	go client()
+	go server()
 
+	var a string
+	fmt.Scanln(&a)
+}
+
+func client() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		q.Name, // queue string
+		"",     // consumer string
+		true,   // autoAck bool
+		false,  // exclusive bool
+		false,  // noLocal bool
+		false,  // noWait bool
+		nil,    //args amqp.Table
+	)
+	failOnError(err, "Failed to consume message")
+
+	for msg := range msgs {
+		log.Printf("Received message: %s", msg.Body)
+	}
+}
+
+func server() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msg := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello RabbitMQ"),
+	}
+
+	for {
+		ch.Publish("", // exchange string
+			q.Name, // routing key
+			false,  // mandatory bool
+			false,  // immedite bool
+			msg,    // amqp.Publishing
+		)
+	}
 }
 
 func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
 	conn, err := amqp.Dial("amqp://guest@localhost:5672")
 	failOnError(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	q, err := ch.QueueDeclare("hello",
+		false, // durable bool
+		false, // autoDelete bool
+		false, // exclusive bool
+		false, // noWait bool
+		nil,   // args amqp.Table
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	return conn, ch, &q
 }
 
-func failOnError(err Error, msg string) {
+func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
 		panic(fmt.Sprintf("%s:%s", msg, err))
