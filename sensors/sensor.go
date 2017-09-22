c5fd713f36ae1fa463172e665199bb8f23c871cf
Romain Bossart <romain.bossart@ge.com>
2017-09-22 20:38:30 +0200

    sensors: app flags

---
+++ sensors/sensor.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+)
+
+var (
+	name     = flag.String("name", "sensor", "name of the sensor")
+	freq     = flag.Uint("freq", 5, "update frequency in cycles/sec")
+	max      = flag.Float64("max", 5., "maximum value for generated readings")
+	min      = flag.Float64("min", 1., "minimum value for generated readings")
+	stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement")
+)
+
+func main() {
+	flag.Parse()
+}
